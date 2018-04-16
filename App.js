d05388aa4ce1a3e742b75ed821c5475c78655a7f
Marcio Nicolau <marcionicolau@me.com>
2018-04-16 17:38:40 -0300

    Add header

--- App.js
+++ App.js
@@ -4,21 +4,28 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { StyleSheet, Text } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header/header';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    return <Text style={styles.welcome}>Welcome to React Native!</Text>;
+    return (
+      <View>
+        <Header />
+        <Text style={styles.welcome}>Welcome to React Native!</Text>;
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   welcome: {
+    flex: 1,
     fontSize: 20,
-    textAlign: "center",
-    color: "tomato",
+    textAlign: 'center',
+    color: 'tomato',
     margin: 10
   }
 });
